d072ae3c58c14d40f3f17554102f8f9ef60df5cf
Renan Ribeiro <renan.ribeiro@schaffen-it.com>
2021-08-09 17:38:12 -0300

    atualizar action

--- App.tsx
+++ App.tsx
@@ -1,17 +1,47 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, Text, View} from 'react-native';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import codePush from 'react-native-code-push';
 
 const App = () => {
+  const handlePress = () => {
+    codePush.sync({
+      updateDialog: codePush.DEFAULT_UPDATE_DIALOG,
+      installMode: codePush.InstallMode.IMMEDIATE,
+    });
+  };
+
   return (
-    <SafeAreaView
-      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" />
       <View>
         <Text>Hello world</Text>
+        <Text>update codepush atualizado</Text>
+
+        <TouchableOpacity onPress={handlePress} style={styles.button}>
+          <Text>Install updates</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    padding: 20,
+    borderWidth: 1,
+  },
+});
 
 export default codePush(App);
